6d70419ce0eb83d9205ffa19af92b6c8a2f206af
shahadathossain26 <shiimran172372@gmail.com>
2023-04-01 21:05:40 +0600

    top 10 data loaded from api

--- server/index.js
+++ server/index.js
@@ -1,15 +1,30 @@
-const express = require('express');
-const cors = require('cors');
+import express from 'express'
+import cors from 'cors';
+import fetch from 'node-fetch';
 const app = express();
 const port = process.env.PORT || 5000;
-
-require('dotenv').config();
+import dotenv from 'dotenv';
+dotenv.config();
 //middlewares
 app.use(cors());
 app.use(express.json());
 
 
+async function getPosts() {
+    const myTickers = await fetch('https://api.wazirx.com/api/v2/tickers');
+    const response = await myTickers.json();
+    const tickers = Object.values(response);
+    const topTickers = tickers.slice(0, 10);
+
+    console.log(topTickers);
+}
+
+
+getPosts();
+
+
 app.get('/', async (req, res) => {
     res.send('Hodlinfo server is running')
 })
+
 app.listen(port, () => console.log(`Hodlinfo running on port ${port}`))
