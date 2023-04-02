f64a743a37dd31ca103586c387ac7d087e119579
shahadathossain26 <shiimran172372@gmail.com>
2023-04-02 16:05:40 +0600

    toggle button light/dark mode added

--- server/index.js
+++ server/index.js
@@ -17,39 +17,46 @@ app.use(express.json());
 
 
 
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.g42knj4.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+const tickersCollection = client.db('hodlinfo').collection('tickers');
+async function getPosts() {
+    const myTickers = await fetch('https://api.wazirx.com/api/v2/tickers');
+    const response = await myTickers.json();
+    const tickers = Object.values(response);
+    const topTickers = tickers.slice(0, 10);
+    console.log(topTickers)
+    for (let i = 0; i < topTickers.length; i++) {
+        const ticker = topTickers[i];
+        const newTicker = {
+            name: ticker.name,
+            last: ticker.last,
+            buy: ticker.buy,
+            sell: ticker.sell,
+            volume: ticker.volume,
+            base_unit: ticker.base_unit,
+
+        }
+        const storedTicker = await tickersCollection.findOne(newTicker);
+        if (storedTicker) {
+            return
+        }
+        const result = await tickersCollection.insertOne(newTicker);
+
+    }
+
+}
+getPosts();
+
 async function run() {
     try {
-        const tickersCollection = client.db('hodlinfo').collection('tickers');
 
-        async function getPosts() {
-            const myTickers = await fetch('https://api.wazirx.com/api/v2/tickers');
-            const response = await myTickers.json();
-            const tickers = Object.values(response);
-            const topTickers = tickers.slice(0, 10);
-            console.log(topTickers)
-            for (let i = 0; i < topTickers.length; i++) {
-                const ticker = topTickers[i];
-                const newTicker = {
-                    name: ticker.name,
-                    last: ticker.last,
-                    buy: ticker.buy,
-                    sell: ticker.sell,
-                    volume: ticker.volume,
-                    base_unit: ticker.base_unit,
 
-                }
-                const storedTicker = await tickersCollection.findOne(newTicker);
-                if (!storedTicker) {
-                    const result = await tickersCollection.insertOne(newTicker);
-                }
 
-            }
-        }
-        getPosts();
+
 
         app.get('/tickers', async (req, res) => {
             const query = {};
